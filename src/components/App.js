dda89344e923391f9817ab0fcffd28a6e0204ca1
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-19 23:18:06 +0300

    Complets the react hooks simple data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+export default function App() {
+  const [randomDog, setRandomDog] = useState(null);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setRandomDog(data.message));
+  }, []);
+
+  if (!randomDog) return <p>Loading...</p>;
+  return (
+    <div>
+      <img src={randomDog} alt="A Random Dog" />
+    </div>
+  );
+}
